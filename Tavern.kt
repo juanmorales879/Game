af596c2959439692d827d2cf6b2319b62f931e0a
Juan Jose Morales <moralesjuan97@outlook.com>
2025-02-05 21:23:27 -0400

    Strings

--- Tavern.kt
+++ Tavern.kt
@@ -1,8 +1,11 @@
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
 
     //using the safe call operator
     var beverage = readLine()?.replaceFirstChar { it.uppercase() }
-// beverage = null
+    // beverage = null
     println(beverage)
 
     // option 1 for dealing with null
@@ -14,6 +17,8 @@ fun main() {
         }
     }
 
+    val beverageServed: String = beverage ?: "Buttered Ale"
+
     println(beverage2)
 
     // option 2 for dealing with null
@@ -24,6 +29,26 @@ fun main() {
         println("I can't do that without crashing - beverage was null!")
     }
 
+    placeOrder()
+    menu("shandy,Dragon's Breath,5.91")
+}
+
+private fun placeOrder() {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+}
+
+private fun menu (menuItem:String){
+    //val data = menuItem.split(',')
+    //val type = data[0]
+    //val name = data[1]
+    //val price = data[2]
+    val (type,name,price) = menuItem.split(',')
+    val message = "Madrigal buys $name ($type) for $price"
+    println(message)
+
 
 }
+
 
